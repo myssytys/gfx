e14aacf196471028cf598574e097de7cc2cb0c44
Mystic Cloud <myssytys@gmail.com>
2024-01-03 18:32:45 +0200

    Add files via upload

--- Tetra.cpp
+++ Tetra.cpp
@@ -15,8 +15,8 @@ class Tetra {
 
 	float cX, cY, cZ;
 	float edge;
-	Vertex* vertex;
-	int *indices;	
+	float* vertex;
+	int* indices;	
 
 	// Constructor
 	Tetra(float cX, float cY, float cZ, float edge) {
@@ -27,32 +27,27 @@ class Tetra {
 		this->cZ = cZ;
 		this->edge = edge;
 
-		vertex = new Vertex[12];
-		indices = (int*)malloc(12);
+		vertex = new float[12];
+		indices = new int[12];
 
-	    vertex=new Vertex{
-			0.0f, edge, 0.0f,
-			edge/2.0f, -edge/2.0f, 0.0f, 
-			0.0f, 0.0f, edge,
-			-edge/2.0f, -edge/2.0f, 0.0f
-			};
+	    vertex[0] = 0.0f;    vertex[1] = edge;        vertex[2] = 0.0f;
+		vertex[3] =	edge/2;  vertex[4] = -edge/2;     vertex[5] = 0.0f;
+		vertex[6] = 0.0f;    vertex[7] = 0.0f;        vertex[8] = edge;
+		vertex[9] = -edge/2; vertex[10] = -edge/2.0f; vertex[11] = 0.0f;
+			
 
 		
-	   indices={
-		 
-		    0, 1, 2,  // Face 0
-            0, 2, 3,  // Face 1
-            0, 3, 1,  // Face 2
-            1, 3, 2   // Face 3
-	   };
-	
+	   indices[0] = 0; indices[1]  = 1; indices[2] = 2;
+	   indices[3] = 0; indices[4]  = 2; indices[5] = 3;
+	   indices[6] = 0; indices[7]  = 3; indices[8] = 1;
+	   indices[9] = 1; indices[10] = 3; indices[11] = 2;
 	
 }
 	
 
 	~Tetra() {
-		delete vertex;
-		delete indices;
+		delete[] vertex;
+		delete[] indices;
 
 	}
 
