4be852256b1f948fde5f41f06f8c7e5ac0d086ca
Mystic Cloud <myssytys@gmail.com>
2024-01-03 18:43:25 +0200

    Add files via upload

--- Tetra.cpp
+++ Tetra.cpp
@@ -15,7 +15,7 @@ class Tetra {
 
 	float cX, cY, cZ;
 	float edge;
-	float* vertex;
+	Vertex* vertex;
 	int* indices;	
 
 	// Constructor
@@ -27,16 +27,14 @@ class Tetra {
 		this->cZ = cZ;
 		this->edge = edge;
 
-		vertex = new float[12];
+		vertex = new Vertex[12];
 		indices = new int[12];
 
-	    vertex[0] = 0.0f;    vertex[1] = edge;        vertex[2] = 0.0f;
-		vertex[3] =	edge/2;  vertex[4] = -edge/2;     vertex[5] = 0.0f;
-		vertex[6] = 0.0f;    vertex[7] = 0.0f;        vertex[8] = edge;
-		vertex[9] = -edge/2; vertex[10] = -edge/2.0f; vertex[11] = 0.0f;
-			
+	    vertex[0].x = 0.0f;    vertex[1].y = edge;        vertex[2].z = 0.0f;
+		vertex[3].x =	edge/2;  vertex[4].y = -edge/2;     vertex[5].z = 0.0f;
+		vertex[6].x = 0.0f;    vertex[7].y = 0.0f;        vertex[8].z = edge;
+		vertex[9].x = -edge/2; vertex[10].y = -edge/2.0f; vertex[11].z = 0.0f;
 
-		
 	   indices[0] = 0; indices[1]  = 1; indices[2] = 2;
 	   indices[3] = 0; indices[4]  = 2; indices[5] = 3;
 	   indices[6] = 0; indices[7]  = 3; indices[8] = 1;
