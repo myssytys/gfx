ec2520ecb066e2aaf0e846453d225e5f81bd547e
Mystic Cloud <myssytys@gmail.com>
2024-01-24 13:49:07 +0200

    Add files via upload

--- Sphere.cpp
+++ Sphere.cpp
@@ -86,22 +86,18 @@ class Sphere {
 
 	std::vector<float> icovertex{
 
-		phi*size, 1.0f*size, 0.0f,
-		phi*size, -1.0f*size, 0.0f,
-		-phi*size, -1.0f*size, 0.0f,
-		-phi*size, 1.0f*size, 0.0f,
-		1.0f*size, 0.0f, phi*size,
-		-1.0f*size, 0.0f, phi*size,
-		-1.0f*size, 0.0f, -phi*size,
-		1.0f*size, 0.0f, -phi*size,
-		0.0f, phi*size, 1.0f*size,
-		0.0f, phi*size -1.0f*size,
-		0.0f, -phi*size, -1.0f*size,
-		0.0f, -phi*size, 1.0f*size
+		-1.0f,  phi,  0.0f,  1.0f,  phi,  0.0f, -1.0f, -phi,  0.0f, 
+		 1.0f, -phi,  0.0f,  0.0f, -1.0f,  phi,  0.0f,  1.0f,  phi, 
+		 0.0f, -1.0f, -phi,  0.0f,  1.0f , -phi, phi,  0.0f, -1.0f, 
+		 phi,  0.0f,  1.0f, -phi,  0.0f, -1.0f, -phi,  0.0f,  1.0f
+	
 	};
 
 	std::vector<int> icoindices{
-
+		0, 11, 5,  0, 5,  1,  0, 1, 7,  0,  7, 10, 0, 10, 11,
+        1, 5,  9,  5, 11, 4,  11,10,2,  10, 7, 6,  7, 1, 8,
+        3, 9,  4,  3, 4,  2,  3, 2, 6,  3,  6, 8,  3, 8, 9,
+        4, 9,  5,  2, 4,  11, 6, 2, 10, 8,  6, 7,  9, 8, 1
 	};
 
 
