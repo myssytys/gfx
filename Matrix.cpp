261219e3418791f78a6d145e98a62cbc6cad08fe
Mystic Cloud <myssytys@gmail.com>
2024-01-25 06:18:39 +0200

    Add files via upload

--- Matrix.cpp
+++ Matrix.cpp
@@ -13,24 +13,47 @@ class Matrix {
     Vector vb;
     Vector vc;
     float* M[12];
+    float* iM[12];
 
-    Matrix();
+        // Default constructor
+    Matrix() {
+            std::vector<float> iM {1.0f, 0.0f, 0.0f, 0.0f,
+                                   0.0f, 1.0f, 0.0f, 0.0f,
+                                   0.0f, 0.0f, 1.0f, 0.0f};
+    };
 
      Matrix(float m11, float m21, float m31, float m41,
             float m12, float m22, float m32, float m42,
             float m13, float m23, float m33, float m43) {
 
+                // Initialize 4x4 matrix
             std::vector<float> M {m11, m21, m31, m41,
                                   m12, m22, m32, m42,
                                   m13, m23, m33, m43 };
 
-           }
+                // Make Identity Matrix
+            std::vector<float> iM {1.0f, 0.0f, 0.0f, 0.0f,
+                                   0.0f, 1.0f, 0.0f, 0.0f,
+                                   0.0f, 0.0f, 1.0f, 0.0f};
 
+
+           }
+    // Matrix init with 3 vectors
     Matrix(const Vector &A, const Vector &B, const Vector &C) {
 
             std::vector<float> M {A.x, B.x, C.x, 1.0f,
                                   A.y, B.y, C.y, 1.0f,
                                   A.z, B.z, C.z, 1.0f};
 
+            std::vector<float> iM {1.0f, 0.0f, 0.0f, 0.0f,
+                                   0.0f, 1.0f, 0.0f, 0.0f,
+                                   0.0f, 0.0f, 1.0f, 0.0f};
+
     }
+
+    // Deconstructor
+    ~Matrix() {
+
+    }
+
 };
--- Vector.cpp
+++ Vector.cpp
@@ -5,23 +5,24 @@ class Vector {
 public:
     float x,y,z;
 
-    Vector(float x, float y, float z) {
-        this->x=x;
-        this->y=y;
-        this->z=z;
-    }
-
+		// Default constructor
 	Vector() {
 		this->x=0.0f;
 		this->y=0.0f;
 		this->z=0.0f;
 	}
-
+		// Initializing constructor
+    Vector(float x, float y, float z) {
+        this->x=x;
+        this->y=y;
+        this->z=z;
+    }
+		// Dot product of 2 vectors
 float dot(const Vector &a, const Vector &b)
 {
 	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
-
+		// Cross product of 2 vectors
 Vector cross(const Vector &a, const Vector &b)
 {
 	return Vector(
@@ -29,18 +30,18 @@ Vector cross(const Vector &a, const Vector &b)
 		a.z * b.x - a.x * b.z,
 		a.x * b.y - a.y * b.x);
 }
-
+	// Return length of vector
 float length(const Vector &a)
 {
 	return std::sqrt(dot(a, a));
 }
-
+	// Get vector normal
 Vector normalize(const Vector &a)
 {
 	const float lrcp = 1.0f / std::sqrt(dot(a, a));
 	return Vector(a.x * lrcp, a.y * lrcp, a.z * lrcp);
 }
-
+	
 Vector operator+(const Vector &a) {
 	Vector res;
 	res.x = a.x+x;
