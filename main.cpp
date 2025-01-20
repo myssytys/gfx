85a0ad57ce2508e7689097d9e27227835acade7c
Mystic Cloud <myssytys@gmail.com>
2025-01-20 19:40:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
