f07e253640ac05ce16c2b17e742c50b447d35d84
Mystic Cloud <myssytys@gmail.com>
2026-08-21 01:43:09 +0300

    Add files via upload

--- Tetra.cpp
+++ Tetra.cpp
@@ -1,33 +1,87 @@
+// TETRAHEDRON CLASS CLOCKWISE WINDING ORDER
+
 #include "Tetra.h"
 
-#include <vector>
+Tetra::Tetra(float edge) {
+  this->edge = edge;
 
+  Data = {
+      // Right-front face
+      // Apex [0]
+      {{0.0f, 1.0f * edge, 0.0f},
+       {0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.5f, 1.0f}},
+      // Base-Right [1]
+      {{edge / 2.0f, -edge / 2.0f, 0.0f},
+       {0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {1.0f, 0.0f}},
+      // Front [2]
+      {{0.0f, 0.0f, 1.0f * edge},
+       {0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.0f, 0.0f}},
 
+      // Left-front face
+      // Apex [3]
+      {{0.0f, 1.0f * edge, 0.0f},
+       {-0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.5f, 1.0f}},
+      // Front [4]
+      {{0.0f, 0.0f, 1.0f * edge},
+       {-0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {1.0f, 0.0f}},
+      // Base-Left [5]
+      {{-edge / 2.0f, -edge / 2.0f, 0.0f},
+       {-0.904534f, 0.301511f, 0.301511f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.0f, 0.0f}},
 
-	Tetra::Tetra(float edge) {
+      // Back face
+      // Apex [6]
+      {{0.0f, 1.0f * edge, 0.0f},
+       {0.0f, 0.0f, -1.0f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.5f, 1.0f}},
+      // Base-Right [7]
+      {{edge / 2.0f, -edge / 2.0f, 0.0f},
+       {0.0f, 0.0f, -1.0f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.0f, 0.0f}},
+      // Base-Left [8]
+      {{-edge / 2.0f, -edge / 2.0f, 0.0f},
+       {0.0f, 0.0f, -1.0f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {1.0f, 0.0f}},
 
-	    std::vector<float> vertex{
-			0.0f, edge, 0.0f,
-			edge/2, -edge/2, 0.0f,
-			0.0f, 0.0f, edge,
-			-edge/2, -edge/2.0f, 0.0f
-		};		
-		
-		std::vector<float> color {
-			1.0f, 1.0f, 1.0f, 1.0f,
-			1.0f, 1.0f, 1.0f, 1.0f,
-			1.0f, 1.0f, 1.0f, 1.0f,
-			1.04, 1.0f, 1.0f, 1.0f
+      // Bottom face
+      // Base-Right [9]
+      {{edge / 2.0f, -edge / 2.0f, 0.0f},
+       {0.0f, -0.894427f, 0.447214f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {1.0f, 0.0f}},
+      // Base-Left [10]
+      {{-edge / 2.0f, -edge / 2.0f, 0.0f},
+       {0.0f, -0.894427f, 0.447214f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.0f, 0.0f}},
+      // Front [11]
+      {{0.0f, 0.0f, 1.0f * edge},
+       {0.0f, -0.894427f, 0.447214f},
+       {1.0f, 1.0f, 1.0f, 1.0f},
+       {0.5f, 1.0f}}};
 
-		};
+  indices = {// Right-front face
+             0, 1, 2,
+             // Left-front face
+             3, 4, 5,
+             // Back face
+             6, 7, 8,
+             // Bottom face
+             9, 10, 11};
+}
 
-		std::vector<int> indices{
-			0,1,2,
-			0,2,3,
-			0,3,1,
-			1,3,2
-		};
-	}
-
-
-
+Tetra::~Tetra() {}
